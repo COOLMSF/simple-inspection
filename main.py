84410cacb90c7dfcbb26f83e7f35f43222c6029c
COOLMSF <coolder.hsl@outlook.com>
2022-11-07 14:38:43 +0800

    tablewidget添加数据并显示

--- main.py
+++ main.py
@@ -65,33 +65,18 @@ class Host():
         return self.passwd
 
 class SSHExecCmdWoker(QThread):
+    conn = None
     signal_ssh_cmd_exec_over = pyqtSignal()
     
-    def __init__(self, mainwindow):
+    def __init__(self, cmd_array:list):
         super(SSHExecCmdWoker, self).__init__()
-        self.mainwindow = mainwindow
-        
-        # 获取全局ssh句柄
-        self.conn = conn
-        
         self.run = True
-        self.cmd = mainwindow.cmd
-        self.cmd_array = mainwindow.cmd_array
         
-        self.signal_ssh_cmd_exec_over.connect(self.update_ui)
-        
-    def update_ui(self):
-        self.mainwindow.te_cmd_output.setText("链接测试通过✅")
-        # ssh连通，打开巡检按钮
-        self.mainwindow.btn_inspection.setEnabled(True)
-        # self.mainwindow.te_cmd_output.setText(connect_msg)
-    
     def run(self):
         cmd_exec_result = self.exec_cmd_array()
         self.signal_ssh_cmd_exec_over.emit(cmd_exec_result)
     
     def exec_cmd_array(self) -> str:
-        self.cmd_array = self.te_cmd_input.toPlainText().split('\n')
         cmd_exec_result = ""
         for self.cmd in self.cmd_array:
             cmd_exec_result += self.exec_cmd(self.cmd)
@@ -174,6 +159,12 @@ class HostInfoInput(Ui_host_info_input.Ui_Form, QWidget):
     host_passwd = None
     host_username = None
     
+    # 发送添加主机信号
+    signal_host_info_input_widget_add = pyqtSignal(str)
+    
+    # host_info_input widget的信号，接收子窗口传过来的IP，账号，密码
+    signal_host_info_input_widget_data = pyqtSignal(dict)
+    
     def __init__(self):
         super(HostInfoInput, self).__init__()
         self.setupUi(self)
@@ -198,9 +189,14 @@ class HostInfoInput(Ui_host_info_input.Ui_Form, QWidget):
         host_info['passwd'] = self.host_passwd
         host_info['username'] = self.host_username
         
-        self.mainwindow = MainWindow()
         # 发送host_info
-        self.mainwindow.signal_host_info_input_widget.emit(host_info)
+        self.signal_host_info_input_widget_data.emit(host_info)
+        
+        # 清空上一次输入的数据
+        self.le_host_ip.setText('')
+        self.le_host_username.setText('')
+        self.le_host_passwd.setText('')
+        
         self.close()
     
     def btn_calcel_clicked(self):
@@ -233,11 +229,8 @@ class MainWindow(UI_mainwidget.Ui_Form, QWidget):
     # 主机列表
     hosts = []
     
-    # host_info_input widget的信号，接收子窗口传过来的IP，账号，密码
-    signal_host_info_input_widget = pyqtSignal(dict)
-    
-    def __init__(self):
-        super(MainWindow, self).__init__()
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.init_ui()
         self.init_slot()
@@ -285,12 +278,12 @@ class MainWindow(UI_mainwidget.Ui_Form, QWidget):
             # init ssh woker
             self.ssh_conn_test_worker = SSHConnTestWorker(self)
             self.ssh_conn_test_worker.start()
-        
         self.showProgregssDialog()
         
     def btn_inspection_clicked(self):
-        self.ssh_exec_cmd_worker = SSHExecCmdWoker()
-        self.ssh_exec_cmd_worker.start()
+        for host in self.hosts:
+            self.ssh_exec_cmd_worker = SSHExecCmdWoker()
+            self.ssh_exec_cmd_worker.start()
         self.showProgregssDialog()
         
     def btn_about_clicked(self):
@@ -298,7 +291,8 @@ class MainWindow(UI_mainwidget.Ui_Form, QWidget):
         
     # top left side UI logic
     def btn_add_host_clicked(self):
-        HostInfoInput().show()
+        # 获取信号
+        self.host_info_input.show()
     
     def btn_del_host_clicked(self):
         selected_items = self.tw_host_info.selectedItems()
@@ -316,30 +310,39 @@ class MainWindow(UI_mainwidget.Ui_Form, QWidget):
         pass
     
     def add_host(self, host_info):
+        print("signal add host recieved")
         self.hosts.append(Host(ip=host_info['ip'], username=host_info['username'], passwd=host_info['passwd']))
-        # 更新UI
-        self.update_ui_host()
-
+        print("add host complete")
         
+        # 操作完成，发送信号
+        print("send update signal")
+        self.host_info_input.signal_host_info_input_widget_add.emit("done")
+
     def del_host(self, ip):
         for host in self.hosts:
             # 按照ip删除主机
             if host.ip == ip:
                 self.hosts.remove(host)
-        self.update_ui_host()
         
     def get_all_hosts(self):
         for host in self.hosts:
             print("Host:%s\nUsername:%s\nPasswd:%s" % host.ip, host.username, host.passwd)
         
     # UI更新部分
-    def update_ui_host(self):
-        for i, host in enumerate(self.hosts):
-            print(i, str(host.ip), host.username, host.passwd)
-            print(self.tw_host_info)
-            self.tw_host_info.setItem(i, 0, QTableWidgetItem(host.ip))
-            self.tw_host_info.setItem(i, 1, QTableWidgetItem(str(host.username)))
-            self.tw_host_info.setItem(i, 2, QTableWidgetItem(str(host.passwd)))
+    def update_ui_host(self, msg):
+        if msg == "done":
+            print("row count from update")
+            print(self.tw_host_info.rowCount())
+            print(self.tw_host_info.columnCount())
+            for i, host in enumerate(self.hosts):
+                print(i, str(host.ip), host.username, host.passwd)
+                print(self.tw_host_info)
+                self.tw_host_info.setItem(i, 0, QTableWidgetItem(host.ip))
+                self.tw_host_info.setItem(i, 1, QTableWidgetItem(host.username))
+                self.tw_host_info.setItem(i, 2, QTableWidgetItem(host.passwd))
+            print("update ui done")
+        else:
+            QMessageBox("asd")
         
     def init_ui(self):
         self.resize(1000, 700)
@@ -350,9 +353,14 @@ class MainWindow(UI_mainwidget.Ui_Form, QWidget):
         self.setFixedSize(self.width(), self.height())
         
         # 设置行数
-        self.tw_host_info.setRowCount(3)
+        self.tw_host_info.setRowCount(10)
         self.tw_host_info.setColumnCount(4)
-    
+        
+        # 子窗口
+        self.host_info_input = HostInfoInput()
+        print("signal")
+        print(self.host_info_input.signal_host_info_input_widget_add)
+        
     # 槽部分
     def init_slot(self):
         # top left side
@@ -366,11 +374,12 @@ class MainWindow(UI_mainwidget.Ui_Form, QWidget):
         
         # buttom side
         self.btn_about.clicked.connect(lambda x: self.btn_about_clicked())
-        self.btn_inspection.clicked.connect(lambda x: self.btn_inspection_clicked())
         self.btn_conn_test.clicked.connect(lambda x: self.btn_conn_test_clicked())
+        self.btn_inspection.clicked.connect(lambda x: self.btn_inspection_clicked())
         
-        # signal
-        self.signal_host_info_input_widget.connect(lambda x:self.add_host(x))
+        # 子窗口信号连接
+        self.host_info_input.signal_host_info_input_widget_data.connect(lambda x: self.add_host(x))
+        self.host_info_input.signal_host_info_input_widget_add.connect(lambda x: self.update_ui_host(x))
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
